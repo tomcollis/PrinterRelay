19cdfc2b7261933f03a417441b37fddb8541b92e
Tom Collis <22452596+tomcollis@users.noreply.github.com>
2022-01-19 12:50:58 +0000

    Add initial post endpoint and replace Hello World with error message.

--- index.js
+++ index.js
@@ -1,16 +1,49 @@
 'use strict';
 
 const express = require('express');
+var app = module.exports = express();
+app.use(express.json());
+var path = require('path');
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World');
+// Error Message
+function error(status, msg) {
+  var err = new Error(msg);
+  err.status = status;
+  return err;
+}
+
+// a simple help guide
+app.get('/', function(req, res){
+  res.status(200);
+  // res.set('Cache-control', `no-store`)
+  // res.sendFile(path.join(__dirname, 'index.html'));
+  res.send({ error: "Hello World" });
 });
+
+// the following, accepts any http posts that contains data
+
+app.post('/p/', function(req, res){
+  if (req.body.length <= 0) {
+    // post is empty
+    res.status(404);
+    res.set('Cache-control', `no-store`)
+    res.send({ error: "no data" });
+    console.log('received: empty request');
+  } else {
+    // post is not empty
+    console.log('receiving data from');
+    res.status(200);
+    res.set('Cache-control', `no-store`)
+    res.send({ success: "data received", body: req.body});
+    console.log('data received and processed');
+  }
+});
+
+// Add ERROR and 404
 
 // Future Command
 // 
