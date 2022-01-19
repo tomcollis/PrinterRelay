04061c78e052956f5232bc3a9e06f097f7ef0f33
Tom Collis <22452596+tomcollis@users.noreply.github.com>
2022-01-19 12:20:00 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-{
-    "name": "docker_web_app",
-    "version": "1.0.0",
-    "description": "Node.js on Docker",
-    "author": "First Last <first.last@example.com>",
-    "main": "server.js",
-    "scripts": {
-      "start": "node index.js"
-    },
-    "dependencies": {
-      "express": "^4.16.1"
-    }
-  }
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+// Future Command
+// 
+// cat you_file.prn | nc -w 1 printer_ip 9100
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
